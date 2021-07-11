72e4b55bc1067fe3b58f4a8dbcca22fbdcc38d0e
NAOYA_MAEDA <n.m.dev.0729@gmail.com>
2021-07-11 20:52:08 +0900

    Initial Commit

---
+++ AVFoundation-Camera-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVFoundation-Camera-App
+//
+//  Created by N. M on 2021/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AVFoundation-Camera-AppTests/AVFoundation_Camera_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  AVFoundation_Camera_AppTests.swift
+//  AVFoundation-Camera-AppTests
+//
+//  Created by N. M on 2021/07/11.
+//
+
+import XCTest
+@testable import AVFoundation_Camera_App
+
+class AVFoundation_Camera_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
